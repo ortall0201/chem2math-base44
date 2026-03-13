ca6ec9ef665f4432c36cb47572acbb91e2de180f
Ortal y <ortalgr@gmail.com>
2026-03-13 17:07:27 +0000

    File changes

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet, Link, useLocation } from "react-router-dom";
 import { LayoutDashboard, BookOpen, FlaskConical, Bot, Menu, X, Zap } from "lucide-react";
 import { Button } from "@/components/ui/button";
